4039115da5d3c19de75a9a01c59e6f07ae8ed80a
to3955 <>
2023-12-03 13:56:49 +0900

    calculation.add

---
+++ src/sample/calculation.java
@@ -0,0 +1,15 @@
+package sample;
+
+public class calculation {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+    System.out.println(5 * 8);
+    System.out.println(9 / 3);
+    System.out.println(10 % 4);
+    System.out.println(5 >= 8);
+    System.out.println(10 != 10);
+    System.out.println(false ? 1 : 0);
+	}
+
+}
