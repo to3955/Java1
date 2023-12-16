c00b0c6d3e67429f7e8370aeac21aaf101818b0f
to3955i <>
2023-12-16 14:33:48 +0900

    git commit -m"test3"

---
+++ src/sample/test3.java
@@ -0,0 +1,14 @@
+package sample;
+
+public class test3 {
+	 public static void main(String[] args) {
+	        // 1から10までの整数を横並びで表示
+	        for (int i = 1; i < 10; i++) {
+	            System.out.print(i + ", ");
+	        }
+
+	        // 10の場合はカンマをつけない
+	        System.out.println(10);
+	    }
+
+}
